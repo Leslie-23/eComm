ed0bd13649eca074a0254782c27bc6e404f2607d
leslie-23 <leslieajayi27@gmail.com>
2024-08-07 12:17:09 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+
+// setting up config file
+dotenv.config({ path: "server/config/config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
