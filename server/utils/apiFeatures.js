fd571f3efe52433186913538f7b95a7f5d19d04c
leslie-23 <leslieajayi27@gmail.com>
2024-08-09 10:32:32 +0000

    advanced filter added (regex)

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -21,9 +21,14 @@ class APIFeatures {
     // removing field from the query
     const removeFields = ["keyword", "limit", "page"];
     removeFields.forEach((el) => delete queryCopy[el]);
-    console.log(queryCopy);
 
-    this.query = this.query.find(queryCopy);
+    // Advanced filter for price,ratings etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+    // console.log(queryCopy);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
