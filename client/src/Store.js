f17990dbd7943eaebe340451adc66cb065b85228
leslie-23 <leslieajayi27@gmail.com>
2024-08-15 14:08:17 +0000

    redux issues fixed(installed redux

--- client/src/Store.js
+++ client/src/Store.js
@@ -1,6 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducers/productReducers";
 
 const reducer = combineReducers({});
+
+let initialState = {
+  // cart items;
+};
+
+const middleware = [thunk];
+const Store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default Store;
